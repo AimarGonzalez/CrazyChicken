bc9fba6187e7802b9ff8b5e744e47f7dee33d7df
Carlos <carlos.lopez@king.com>
2016-03-20 21:10:39 +0100

    new event trigger script

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Events : MonoBehaviour {
+
+    // Use this for initialization
+    public float Lasteventtime;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time - Lasteventtime == 10)
+        {
+            Lasteventtime = Time.time;
+
+            //Choose event
+            float eventRoulette = Random.Range(0f, 2.99f);
+            int eventChosen = Mathf.CeilToInt(eventRoulette);
+
+            if (eventChosen == 0)
+            {
+                //Trigger event
+            }
+
+
+            else if (eventChosen == 1)
+            {
+                //Trigger event 
+            }
+
+            else {
+                //trigger event
+            }
+
+        }
+
+    }
+}
